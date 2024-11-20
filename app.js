1437d13dd60030a70dc8e67f73ded58a208bf81e
ilaz000 <ilazk308@gmail.com>
2024-11-20 09:42:18 +0600

    create a new project

---
+++ app.js
@@ -0,0 +1,31 @@
+// var let const
+// streing, number, boolean, array, object
+
+// const brother='Chungyz'
+// const friend='Adahan'
+
+// const brothers=['Chungyz', 'Adahan', 'Akyl']
+
+// console.log(brothers);
+
+// brothers.unshift('Dastan')
+// console.log(brothers);
+
+const students={
+    name:'Iliaz',
+    age:'12',
+    game:'computer game', 
+    family: '14 people.',
+    study:'English.',
+    bestfriend:'Adahan.',
+    hobbies:['listen to music', 'swim','paint'],
+    
+}
+
+console.log('My name is', students.name, 'I am', students.age, 'years old.', 'I like play', students.game, 'My family consists of', students.family, 'I love study', students.study, 'My friend', students.bestfriend,  'and my like hobbies', students.hobbies);[0];
+
+
+
+
+
+
